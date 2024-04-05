0663575da5728804c5560a5269d82c50aa6c05aa
riadfrancoq <campusricardo2023@gmail.com>
2024-04-05 18:58:33 -0500

    feat: :tada: Start the course, minute: 50:47

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,38 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    spacing: {
+      '1': '100px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
     extend: {
+      colors: {
+        vuejs: {
+          100: "#49e659",
+          932: "#434252"
+        } 
+      },
+      fontSize: {
+        base: '18px',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+
   },
   plugins: [],
 };
