5c29c483a07ec8709e7fb0c3ac79de80816e9c9c
riadfrancoq <campusricardo2023@gmail.com>
2024-04-08 13:20:07 -0500

    feat: :sparkles: Typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    <h1 className="text-3xl"> Title 1</h1>
-    <h2 className="text-2xl"> Title 2</h2>
-    <h3 className="text-xl"> Title 3</h3>
-    <p className="text-base">A regular paragraph</p>
+    <p className="break-all"> This is a long sentence with a lot of words to explain 
+    lamamadelamamadelamamdelamamadelamadelamaa
+    that truncate will truncate my sentence</p>
+    {/* {<h1 className="text-3xl italic underline decoration-red-500 decoration-double"> Title 1</h1>
+    <h2 className="text-2xl line-through decoration-dotted"> Title 2</h2>
+    <h3 className="text-xl italic underline decoration-wavy decoration-1 underline-offset-8"> Title 3</h3>
+    <p className="text-base uppercase">A regular paragraph</p>
     <p className="text-sm">A description paragraph</p>
-    <p className="text-xs note"> A little note </p>
+    <p className="text-xs note"> A little note </p>} */}
    </div>
 
   );
