697d7d9c0f8117ddf1a944053b49c376d3690c06
riadfrancoq <campusricardo2023@gmail.com>
2024-04-15 18:03:11 -0500

    feat: :sparkles: Flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div>
-
-    <h1 className="mb-4"> First Title </h1>
-    <h2 className="mb-3"> Second Title </h2>
-    <h3 className="mb-2"> Third Title </h3>
-    <p className="mb-1">paragraph</p>
-
+  <div className="Parent">
+    <div> Header </div>
+    <div className="flex items-baseline">
+      <div className="pt-2 pb-6 bg-sky-500">01</div>
+      <div className="pt-8 pb-12 bg-red-500">02</div>
+      <div className="pt-12 pb-4 bg-green-500">03</div>
+    </div>
   </div>
   
 );
