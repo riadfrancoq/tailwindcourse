07f051665725589cc8f62e606a960a432b4bff3f
riadfrancoq <campusricardo2023@gmail.com>
2024-04-16 12:35:48 -0500

    feat: :sparkles: Grids

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="Parent">
-    <div> Header </div>
-    <div className="flex items-baseline">
-      <div className="pt-2 pb-6 bg-sky-500">01</div>
-      <div className="pt-8 pb-12 bg-red-500">02</div>
-      <div className="pt-12 pb-4 bg-green-500">03</div>
+  <div className="h-screen text-white Parent bg-slate-900">
+    <div className="container mx-auto">
+      <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-3 gap-4 ">
+        <div className="p-6 rounded-lg bg-sky-500 col-span-2">01</div>
+        <div className="p-6 rounded-lg bg-sky-500 col-span-2">02</div>
+        <div className="p-6 rounded-lg bg-sky-500">03</div>
+        <div className="p-6 rounded-lg bg-sky-500">04</div>
+        <div className="p-6 rounded-lg bg-sky-500 ">05</div>
+        <div className="p-6 rounded-lg bg-sky-500 col-span-2">06</div>
+        <div className="p-6 rounded-lg bg-sky-500">07</div>
+        <div className="p-6 rounded-lg bg-sky-500">08</div>
+        <div className="p-6 rounded-lg bg-sky-500">09</div>
+        <div className="p-6 rounded-lg bg-sky-500 col-span-2">10</div>
+        <div className="p-6 rounded-lg bg-sky-500">11</div>
+        <div className="p-6 rounded-lg bg-sky-500">12</div>
+
+      </div>
+
     </div>
   </div>
   
