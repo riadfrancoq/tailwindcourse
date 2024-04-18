e3b20e7a5b98e34fb89319747d56a71655e96a05
riadfrancoq <campusricardo2023@gmail.com>
2024-04-18 12:34:30 -0500

    feat: :sparkles: Borders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
   <div className="h-screen text-white Parent bg-slate-900">
-    <div className="container">
-      
-    <img className="w-64 h-64 object-none object-right-bottom" src="https://images.unsplash.com/photo-1536663815808-535e2280d2c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80" alt="" />
 
-      </div>
+    <button className="ring-offset-2 ring-4"> Button C </button>
+
   </div>
   
 );
 }
 
-// search for windows keys 
