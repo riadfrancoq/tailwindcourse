373addb8fb4feead18ade07b69d87d744b5e6ebf
riadfrancoq <campusricardo2023@gmail.com>
2024-04-18 20:10:13 -0500

    feat: :sparkles: Effects and Filters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div className="h-screen text-white Parent bg-slate-900">
-
-    <button className="ring-offset-2 ring-4"> Button C </button>
-
+  <div className="h-screen p-4 Parent">
+    <div className="inline-block p-4 text-black bg-white border rounded-lg shadow-md">
+      <img src="https://images.unsplash.com/photo-1536663815808-535e2280d2c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80" className="w-64 mb-4 sepia" alt="" />
+      <h1 className="text-2xl"> Hello </h1>
+      <p className="mb-4"> This is my div </p>
+      <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 text-white hover:opacity-50"> Say Hello </button>
+    </div>
   </div>
   
 );
